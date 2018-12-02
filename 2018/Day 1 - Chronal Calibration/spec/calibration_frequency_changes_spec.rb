5cdc0858bad60eea000f3fb3ec928ab95c483aeb
Orien Madgwick <_@orien.io>
2018-12-02 14:37:49 +1100

    Calculate repeated frequency and resulting frequency

--- 2018/Day 1 - Chronal Calibration/spec/calibration_frequency_changes_spec.rb
+++ 2018/Day 1 - Chronal Calibration/spec/calibration_frequency_changes_spec.rb
@@ -3,11 +3,31 @@ require 'calibration_frequency_changes'
 RSpec.describe CalibrationFrequencyChanges do
   subject(:calibration_frequency_changes) { described_class.new(filename) }
 
-  let(:filename) { 'spec/4_changes.txt' }
-
   describe 'each' do
+    let(:filename) { 'spec/4_changes.txt' }
+
     it 'yields each frequency change' do
       expect { |block| calibration_frequency_changes.each(&block) }.to yield_successive_args(15, -6, 5, -15)
+    end
+  end
+
+  describe 'resulting_frequency' do
+    subject(:resulting_frequency) { calibration_frequency_changes.resulting_frequency }
+
+    let(:filename) { 'spec/4_changes.txt' }
+
+    it 'identifies the frequency' do
+      expect(resulting_frequency).to eq(-1)
+    end
+  end
+
+  describe 'repeated_frequency' do
+    subject(:repeated_frequency) { calibration_frequency_changes.repeated_frequency }
+
+    let(:filename) { 'spec/repeated_frequency_14.txt' }
+
+    it 'identifies the frequency' do
+      expect(repeated_frequency).to eq(14)
     end
   end
 end
