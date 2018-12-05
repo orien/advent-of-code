83aca6e57df090c8a801e637e7ff01204601c35a
Orien Madgwick <_@orien.io>
2018-12-05 20:12:00 +1100

    Reformat

--- 2018-ruby/lib/aoc/day1.rb
+++ 2018-ruby/lib/aoc/day1.rb
@@ -7,14 +7,15 @@ module AOC
     end
 
     def self.part2(input)
-      _change, state = input.map(&:to_i)
-                            .cycle
-                            .each_with_object({freq: 0, past_freq: Set[0]})
-                            .find { |change, state|
-                              state[:freq] += change
-                              !state[:past_freq].add?(state[:freq])
-                            }
-      state[:freq]
+      _change, state =
+        input.map(&:to_i)
+        .cycle
+        .each_with_object({frequency: 0, past_frequencies: Set[0]})
+        .find { |change, state|
+          state[:frequency] += change
+          !state[:past_frequencies].add?(state[:frequency])
+      }
+      state[:frequency]
     end
   end
 end
