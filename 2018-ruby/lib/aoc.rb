04bf069c4345d39d3db31eb73083ce1770171176
Orien Madgwick <_@orien.io>
2018-12-15 23:21:33 +1100

    Main entrypoint can figure out how many solutions we have

--- 2018-ruby/lib/aoc.rb
+++ 2018-ruby/lib/aoc.rb
@@ -1,19 +1,20 @@
-Dir.glob(__dir__ + '/aoc/day*.rb').each { |f| require f }
-
 module AOC
+  SOLUTIONS = Dir.glob(__dir__ + '/aoc/day*.rb').freeze
+  SOLUTIONS.each { |f| require f }
+
   def self.celebrate
-    (1..11).each do |day|
+    (1..SOLUTIONS.count).each do |day|
       solution = AOC.const_get("Day#{day}")
       puts <<~MESSAGE
         Day ##{day}
-          part 1: #{solution.part1(file(day))}
-          part 2: #{solution.part2(file(day))}
+          part 1: #{solution.part1(input(day))}
+          part 2: #{solution.part2(input(day))}
 
       MESSAGE
     end
   end
 
-  def self.file(day)
+  def self.input(day)
     File.new("input/day#{day}.txt", 'r')
   end
 end
