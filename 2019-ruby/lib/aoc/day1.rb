a17cf5848a1651633e717420254020d6780644e2
Orien Madgwick <_@orien.io>
2019-12-11 04:28:06 +1100

    Day 1, part 1

---
+++ 2019-ruby/lib/aoc/day1.rb
@@ -0,0 +1,11 @@
+module AOC
+  module Day1
+    def self.part1(input)
+      input.map { |i| i.to_i / 3 - 2 }.sum
+    end
+
+    def self.part2(input)
+
+    end
+  end
+end
---
+++ 2019-ruby/spec/aoc/day1_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe AOC::Day1 do
+  describe 'part1' do
+    subject(:part1) { described_class.part1(StringIO.new(input)) }
+
+    context 'given "12"' do
+      let(:input) { "12\n" }
+      it { should eq 2 }
+    end
+
+    context 'given "14"' do
+      let(:input) { "14\n" }
+      it { should eq 2 }
+    end
+
+    context 'given "1969"' do
+      let(:input) { "1969\n" }
+      it { should eq 654 }
+    end
+
+    context 'given "100756"' do
+      let(:input) { "100756\n" }
+      it { should eq 33583 }
+    end
+  end
+
+  describe 'part2' do
+    subject(:part2) { described_class.part2(StringIO.new(input)) }
+  end
+end
