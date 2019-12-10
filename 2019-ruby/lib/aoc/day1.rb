1b26c7e45e960a89afbd0c72daa8e32fde4af895
Orien Madgwick <_@orien.io>
2019-12-11 04:55:01 +1100

    Day 1, part 2

--- 2019-ruby/lib/aoc/day1.rb
+++ 2019-ruby/lib/aoc/day1.rb
@@ -1,11 +1,24 @@
 module AOC
   module Day1
     def self.part1(input)
-      input.map { |i| i.to_i / 3 - 2 }.sum
+      input.map(&:to_i).map(&method(:fuel_for_mass)).sum
     end
 
     def self.part2(input)
+      input.map(&:to_i).map(&method(:fuel_for_mass_plus_fuel)).sum
+    end
 
+    def self.fuel_for_mass(mass)
+      fuel = mass / 3 - 2
+    end
+
+    def self.fuel_for_mass_plus_fuel(mass, fuel = 0)
+      extra_fuel = fuel_for_mass(mass)
+      if extra_fuel.positive?
+        fuel_for_mass_plus_fuel(extra_fuel, fuel + extra_fuel)
+      else
+        fuel
+      end
     end
   end
 end
--- 2019-ruby/spec/aoc/day1_spec.rb
+++ 2019-ruby/spec/aoc/day1_spec.rb
@@ -25,5 +25,20 @@ RSpec.describe AOC::Day1 do
 
   describe 'part2' do
     subject(:part2) { described_class.part2(StringIO.new(input)) }
+
+    context 'given "14"' do
+      let(:input) { "14\n" }
+      it { should eq 2 }
+    end
+
+    context 'given "1969"' do
+      let(:input) { "1969\n" }
+      it { should eq 966 }
+    end
+
+    context 'given "100756"' do
+      let(:input) { "100756\n" }
+      it { should eq 50346 }
+    end
   end
 end
