ed2b69045be00470f0c7db14501d46576ba6db95
Orien Madgwick <_@orien.io>
2019-12-11 11:25:54 +1100

    Day 4, part 2

--- 2019-ruby/lib/aoc/day4.rb
+++ 2019-ruby/lib/aoc/day4.rb
@@ -6,12 +6,20 @@ module AOC
     end
 
     def self.part2(input)
+      from, to = input.readline.split("-").map(&:to_i)
+      (from..to).select(&method(:valid2?)).count
     end
 
     def self.valid?(password)
       consecutive_digits = password.digits.each_cons(2).to_a
       valid = consecutive_digits.all? { |(num1, num2)| num1 >= num2 } &&
         consecutive_digits.any? { |(num1, num2)| num1 == num2 }
+    end
+
+    def self.valid2?(password)
+      digits = password.digits
+      valid = digits.each_cons(2).all? { |(num1, num2)| num1 >= num2 } &&
+        digits.slice_when { |num1, num2| num1 != num2 }.any? { |group| group.size == 2 }
     end
   end
 end
--- 2019-ruby/spec/aoc/day4_spec.rb
+++ 2019-ruby/spec/aoc/day4_spec.rb
@@ -8,6 +8,49 @@ RSpec.describe AOC::Day4 do
     end
   end
 
+  describe 'part2' do
+    subject(:part2) { described_class.part2(StringIO.new(input)) }
+
+    context 'given "156218-652527"' do
+      let(:input) { "156218-652527\n" }
+      it { should eq 1148 }
+    end
+  end
+
+  describe 'valid2?' do
+    subject(:valid2?) { described_class.valid2?(password) }
+
+    context 'given 112233' do
+      let(:password) { 112233 }
+      it { should be true }
+    end
+
+    context 'given 123444' do
+      let(:password) { 123444 }
+      it { should be false }
+    end
+
+    context 'given 111122' do
+      let(:password) { 111122 }
+      it { should be true }
+    end
+
+    context 'given 111111' do
+      let(:password) { 111111 }
+      it { should be false }
+    end
+
+    context 'given 223450' do
+      let(:password) { 223450 }
+      it { should be false }
+    end
+
+    context 'given 123789' do
+      let(:password) { 123789 }
+      it { should be false }
+    end
+  end
+
   describe 'valid?' do
     subject(:valid?) { described_class.valid?(password) }
 
