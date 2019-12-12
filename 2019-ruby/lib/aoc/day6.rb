0e16fd738d13ce70e96dcb0907b8eefec3d08dda
Orien Madgwick <_@orien.io>
2019-12-12 23:21:05 +1100

    Day 6, part 2

--- 2019-ruby/lib/aoc/day6.rb
+++ 2019-ruby/lib/aoc/day6.rb
@@ -1,19 +1,48 @@
 module AOC
   module Day6
     def self.part1(input)
-      nodes = input.map { |line| line.chomp.split(")") }
-        .reduce(Hash.new { |h, k| h[k] = [] }) { |h, (a, b)|
-          h[a] << h[b]
-          h
-        }
-      traverse(nodes["COM"], 0)
+      traverse(nodes(input)["COM"], 0)
     end
 
     def self.part2(input)
+      n = nodes(input)
+      santa = n["SAN"]
+      you = n["YOU"]
+      santas_orbit_path = ancestors(santa)
+      your_orbit_path = ancestors(you) { |node| santas_orbit_path.include?(node) }
+      santas_orbit_path.index(your_orbit_path.last) + your_orbit_path.size - 1
+    end
+
+    def self.nodes(input)
+      input.map { |line| line.chomp.split(")") }
+        .reduce(Hash.new { |h, k| h[k] = Node.new(k) }) { |h, (a, b)|
+          h[a].children << h[b]
+          h[b].parent = h[a]
+          h
+        }
+    end
+
+    def self.ancestors(node, acc = [], &block)
+      if node.parent.nil? || block&.call(node)
+        acc
+      else
+        ancestors(node.parent, acc + [node.parent], &block)
+      end
     end
 
     def self.traverse(node, depth)
-      depth + node.reduce(0) { |sum, child| sum + traverse(child, depth + 1) }
+      depth + node.children.reduce(0) { |sum, child| sum + traverse(child, depth + 1) }
+    end
+
+    class Node
+      def initialize(name)
+        @name = name
+        @parent = nil
+        @children = []
+      end
+
+      attr_reader :name, :children
+      attr_accessor :parent
     end
   end
 end
--- 2019-ruby/spec/aoc/day6_spec.rb
+++ 2019-ruby/spec/aoc/day6_spec.rb
@@ -19,4 +19,27 @@ RSpec.describe AOC::Day6 do
       it { should eq 42 }
     end
   end
+
+  describe 'part2' do
+    subject(:part2) { described_class.part2(StringIO.new(input)) }
+
+    context 'given "COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L K)YOU I)SAN"' do
+      let(:input) { <<~INPUT }
+        COM)B
+        B)C
+        C)D
+        D)E
+        E)F
+        B)G
+        G)H
+        D)I
+        E)J
+        J)K
+        K)L
+        K)YOU
+        I)SAN
+      INPUT
+      it { should eq 4 }
+    end
+  end
 end
