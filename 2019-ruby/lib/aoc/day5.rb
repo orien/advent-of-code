2c06b6a3893727e067bf765918ae5b4033c32820
Orien Madgwick <_@orien.io>
2019-12-13 17:21:15 +1100

    Day 5: input and output to functions rather than stdin and stdout

--- 2019-ruby/lib/aoc/day5.rb
+++ 2019-ruby/lib/aoc/day5.rb
@@ -1,84 +1,83 @@
 module AOC
   module Day5
     def self.part1(input)
-      process(program(input), 0)
-      nil
+      state, out = process(program(input), 0, [1])
+      out.join
     end
 
     def self.part2(input)
-      process(program(input), 0)
-      nil
+      state, out = process(program(input), 0, [5])
+      out.join
     end
 
     def self.program(input)
       input.first.split(",").map(&:to_i)
     end
 
-    def self.process(state, position)
+    def self.process(state, position, input, output = [])
       opcode = state[position] % 100
       parameter_modes = (state[position] / 100).digits
       case opcode
-      when 1 then add(state, position, parameter_modes)
-      when 2 then multiply(state, position, parameter_modes)
-      when 3 then input(state, position)
-      when 4 then output(state, position, parameter_modes)
-      when 5 then jump_if_true(state, position, parameter_modes)
-      when 6 then jump_if_false(state, position, parameter_modes)
-      when 7 then less_than(state, position, parameter_modes)
-      when 8 then equals(state, position, parameter_modes)
-      when 99 then state
+      when 1 then add(state, position, parameter_modes, input, output)
+      when 2 then multiply(state, position, parameter_modes, input, output)
+      when 3 then input(state, position, input, output)
+      when 4 then output(state, position, parameter_modes, input, output)
+      when 5 then jump_if_true(state, position, parameter_modes, input, output)
+      when 6 then jump_if_false(state, position, parameter_modes, input, output)
+      when 7 then less_than(state, position, parameter_modes, input, output)
+      when 8 then equals(state, position, parameter_modes, input, output)
+      when 99 then [state, output]
       else
         raise "Unkown Opcode '#{opcode}' at #{position}"
       end
     end
 
-    def self.add(state, position, parameter_modes)
+    def self.add(state, position, parameter_modes, input, output)
       operand1, operand2 = parameter_values(state, position, parameter_modes, 2)
       new_state = store(state, state[position + 3], operand1 + operand2)
-      process(new_state, position + 4)
+      process(new_state, position + 4, input, output)
     end
 
-    def self.multiply(state, position, parameter_modes)
+    def self.multiply(state, position, parameter_modes, input, output)
       operand1, operand2 = parameter_values(state, position, parameter_modes, 2)
       new_state = store(state, state[position + 3], operand1 * operand2)
-      process(new_state, position + 4)
+      process(new_state, position + 4, input, output)
     end
 
-    def self.input(state, position)
-      new_state = store(state, state[position + 1], gets.to_i)
-      process(new_state, position + 2)
+    def self.input(state, position, (first, *input), output)
+      new_state = store(state, state[position + 1], first)
+      process(new_state, position + 2, input, output)
     end
 
-    def self.output(state, position, parameter_modes)
+    def self.output(state, position, parameter_modes, input, output)
       param, = parameter_values(state, position, parameter_modes)
-      print param
-      process(state, position + 2)
+      process(state, position + 2, input, output + [param])
     end
 
-    def self.jump_if_true(state, position, parameter_modes)
+    def self.jump_if_true(state, position, parameter_modes, input, output)
       param1, param2 = parameter_values(state, position, parameter_modes, 2)
       new_position = param1 != 0 ? param2 : position + 3
-      process(state, new_position)
+      process(state, new_position, input, output)
     end
 
-    def self.jump_if_false(state, position, parameter_modes)
+    def self.jump_if_false(state, position, parameter_modes, input, output)
       param1, param2 = parameter_values(state, position, parameter_modes, 2)
       new_position = param1 == 0 ? param2 : position + 3
-      process(state, new_position)
+      process(state, new_position, input, output)
     end
 
-    def self.less_than(state, position, parameter_modes)
+    def self.less_than(state, position, parameter_modes, input, output)
       param1, param2 = parameter_values(state, position, parameter_modes, 2)
       lt = param1 < param2 ? 1 : 0
       new_state = store(state, state[position + 3], lt)
-      process(new_state, position + 4)
+      process(new_state, position + 4, input, output)
     end
 
-    def self.equals(state, position, parameter_modes)
+    def self.equals(state, position, parameter_modes, input, output)
       param1, param2 = parameter_values(state, position, parameter_modes, 2)
       e = param1 == param2 ? 1 : 0
       new_state = store(state, state[position + 3], e)
-      process(new_state, position + 4)
+      process(new_state, position + 4, input, output)
     end
 
     def self.store(state, position, value)
--- 2019-ruby/spec/aoc/day5_spec.rb
+++ 2019-ruby/spec/aoc/day5_spec.rb
@@ -1,49 +1,50 @@
 RSpec.describe AOC::Day5 do
   describe 'process' do
-    subject(:process) { described_class.process(state, 0) }
+    subject(:process) { described_class.process(state, 0, input) }
+    let(:input) { [] }
 
     describe 'add' do
       context 'given [1,0,0,0,99]' do
         let(:state) { [1,0,0,0,99] }
-        it { should eq [2,0,0,0,99] }
+        it { should eq [[2,0,0,0,99], []] }
       end
     end
 
     describe 'multiply' do
       context 'given [2,3,0,3,99]'do
         let(:state) { [2,3,0,3,99] }
-        it { should eq [2,3,0,6,99] }
+        it { should eq [[2,3,0,6,99], []] }
       end
 
       context 'given [2,4,4,5,99,0]' do
         let(:state) { [2,4,4,5,99,0] }
-        it { should eq [2,4,4,5,99,9801] }
+        it { should eq [[2,4,4,5,99,9801], []] }
       end
     end
 
     describe 'combinations' do
       context 'given [1,1,1,4,99,5,6,0,99]' do
         let(:state) { [1,1,1,4,99,5,6,0,99] }
-        it { should eq [30,1,1,4,2,5,6,0,99] }
+        it { should eq [[30,1,1,4,2,5,6,0,99], []] }
       end
 
       context 'given [1,9,10,3,2,3,11,0,99,30,40,50]' do
         let(:state) { [1,9,10,3,2,3,11,0,99,30,40,50] }
-        it { should eq [3500,9,10,70,2,3,11,0,99,30,40,50] }
+        it { should eq [[3500,9,10,70,2,3,11,0,99,30,40,50], []] }
       end
     end
 
     describe 'parameter modes' do
       context 'given [1002,4,3,4,33,99]' do
         let(:state) { [1002, 4, 3, 4, 33, 99] }
-        it { should eq [1002, 4, 3, 4, 99, 99] }
+        it { should eq [[1002, 4, 3, 4, 99, 99], []] }
       end
     end
 
     describe 'output' do
       context 'given [4,3,99,100]' do
         let(:state) { [4, 3, 99, 100] }
-        specify { expect { process }.to output("100").to_stdout }
+        it { should eq [[4, 3, 99, 100], [100]] }
       end
     end
 
@@ -51,28 +52,28 @@ RSpec.describe AOC::Day5 do
       context 'given [3,9,8,9,10,9,4,9,99,-1,8]' do
         let(:state) { [3,9,8,9,10,9,4,9,99,-1,8] }
 
-        context 'input "8"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("8\n") }
-          specify { expect { process }.to output("1").to_stdout }
+        context 'input [8]' do
+          let(:input) { [8] }
+          it { should eq [[3,9,8,9,10,9,4,9,99,1,8], [1]] }
         end
 
-        context 'input "9"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("9\n") }
-          specify { expect { process }.to output("0").to_stdout }
+        context 'input [9]' do
+          let(:input) { [9] }
+          it { should eq [[3,9,8,9,10,9,4,9,99,0,8], [0]] }
         end
       end
 
       context 'given [3,3,1108,-1,8,3,4,3,99]' do
         let(:state) { [3,3,1108,-1,8,3,4,3,99] }
 
-        context 'input "8"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("8\n") }
-          specify { expect { process }.to output("1").to_stdout }
+        context 'input [8]' do
+          let(:input) { [8] }
+          it { should eq [[3,3,1108,1,8,3,4,3,99], [1]] }
         end
 
-        context 'input "9"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("9\n") }
-          specify { expect { process }.to output("0").to_stdout }
+        context 'input [9]' do
+          let(:input) { [9] }
+          it { should eq [[3,3,1108,0,8,3,4,3,99], [0]] }
         end
       end
     end
@@ -81,28 +82,28 @@ RSpec.describe AOC::Day5 do
       context 'given [3,9,7,9,10,9,4,9,99,-1,8]' do
         let(:state) { [3,9,7,9,10,9,4,9,99,-1,8] }
 
-        context 'input "8"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("8\n") }
-          specify { expect { process }.to output("0").to_stdout }
+        context 'input [8]' do
+          let(:input) { [8] }
+          it { should eq [[3,9,7,9,10,9,4,9,99,0,8], [0]] }
         end
 
-        context 'input "7"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("7\n") }
-          specify { expect { process }.to output("1").to_stdout }
+        context 'input [7]' do
+          let(:input) { [7] }
+          it { should eq [[3,9,7,9,10,9,4,9,99,1,8], [1]] }
         end
       end
 
       context 'given [3,3,1107,-1,8,3,4,3,99]' do
         let(:state) { [3,3,1107,-1,8,3,4,3,99] }
 
-        context 'input "8"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("8\n") }
-          specify { expect { process }.to output("0").to_stdout }
+        context 'input [8]' do
+          let(:input) { [8] }
+          it { should eq [[3,3,1107,0,8,3,4,3,99], [0]] }
         end
 
-        context 'input "7"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("7\n") }
-          specify { expect { process }.to output("1").to_stdout }
+        context 'input [7]' do
+          let(:input) { [7] }
+          it { should eq [[3,3,1107,1,8,3,4,3,99], [1]] }
         end
       end
     end
@@ -111,28 +112,28 @@ RSpec.describe AOC::Day5 do
       context 'given [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]' do
         let(:state) { [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9] }
 
-        context 'input "0"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("0\n") }
-          specify { expect { process }.to output("0").to_stdout }
+        context 'input [0]' do
+          let(:input) { [0] }
+          it { should eq [[3,12,6,12,15,1,13,14,13,4,13,99,0,0,1,9], [0]] }
         end
 
-        context 'input "1"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("1\n") }
-          specify { expect { process }.to output("1").to_stdout }
+        context 'input [1]' do
+          let(:input) { [1] }
+          it { should eq [[3,12,6,12,15,1,13,14,13,4,13,99,1,1,1,9], [1]] }
         end
       end
 
       context 'given [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]' do
         let(:state) { [3,3,1105,-1,9,1101,0,0,12,4,12,99,1] }
 
-        context 'input "0"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("0\n") }
-          specify { expect { process }.to output("0").to_stdout }
+        context 'input [0]' do
+          let(:input) { [0] }
+          it { should eq [[3,3,1105,0,9,1101,0,0,12,4,12,99,0], [0]] }
         end
 
-        context 'input "1"' do
-          before { allow(AOC::Day5).to receive(:gets).and_return("1\n") }
-          specify { expect { process }.to output("1").to_stdout }
+        context 'input [1]' do
+          let(:input) { [1] }
+          it { should eq [[3,3,1105,1,9,1101,0,0,12,4,12,99,1], [1]] }
         end
       end
     end
