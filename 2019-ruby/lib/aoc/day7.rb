1aa8930db7ae1e27e37a1a37518271c4d3994aa7
Orien Madgwick <_@orien.io>
2019-12-13 23:18:10 +1100

    Day 7, part 2

--- 2019-ruby/lib/aoc/day7.rb
+++ 2019-ruby/lib/aoc/day7.rb
@@ -1,16 +1,128 @@
 module AOC
   module Day7
     def self.part1(input)
-      state = Day5.program(input)
+      data = input.first.split(",").map(&:to_i)
       (0..4).to_a.permutation.map { |phase_settings|
-        phase_settings.reduce(0) { |signal, phase_setting|
-          _, out = Day5.process(state, 0, [phase_setting, signal])
-          out.first
-        }
+        io = Array.new(5) { |n| [phase_settings[n]] }
+        io.first << 0
+        output = []
+        programs = Array.new(5) { |n| Program.new(data, io[n], io[n + 1] || output ) }
+        programs.each(&:process)
+        output.first
       }.max
     end
 
     def self.part2(input)
+      data = input.first.split(",").map(&:to_i)
+      (5..9).to_a.permutation.map { |phase_settings|
+        io = Array.new(5) { |n| [phase_settings[n]] }
+        io.first << 0
+        programs = Array.new(5) { |n| Program.new(data, io[n], io[(n + 1) % 5]) }
+        programs.each(&:process) until programs.all?(&:finished?)
+        io.first.first
+      }.max
+    end
+
+    class Program
+      def initialize(data, input, output)
+        @data = Array.new(data)
+        @input = input
+        @output = output
+        @position = 0
+        @finished = false
+      end
+
+      attr_reader :data, :input, :output
+
+      def process
+        opcode = @data[@position] % 100
+        @parameter_modes = (@data[@position] / 100).digits
+        case opcode
+        when 1 then add
+        when 2 then multiply
+        when 3 then input!
+        when 4 then output!
+        when 5 then jump_if_true
+        when 6 then jump_if_false
+        when 7 then less_than
+        when 8 then equals
+        when 99
+          @finished = true
+        else
+          raise "Unkown Opcode '#{opcode}' at #{position}"
+        end
+      end
+
+      def add
+        store(@data[@position + 3], param1 + param2)
+        @position += 4
+        process
+      end
+
+      def multiply
+        store(@data[@position + 3], param1 * param2)
+        @position += 4
+        process
+      end
+
+      def input!
+        return if @input.empty?
+        store(@data[@position + 1], @input.shift)
+        @position += 2
+        process
+      end
+
+      def output!
+        @output << param1
+        @position += 2
+        process
+      end
+
+      def jump_if_true
+        @position = param1 != 0 ? param2 : @position + 3
+        process
+      end
+
+      def jump_if_false
+        @position = param1 == 0 ? param2 : @position + 3
+        process
+      end
+
+      def less_than
+        store(@data[@position + 3], param1 < param2 ? 1 : 0)
+        @position += 4
+        process
+      end
+
+      def equals
+        store(@data[@position + 3], param1 == param2 ? 1 : 0)
+        @position += 4
+        process
+      end
+
+      def store(position, value)
+        @data[position] = value
+      end
+
+      def param1
+        parameter_value(@parameter_modes[0], @position + 1)
+      end
+
+      def param2
+        parameter_value(@parameter_modes[1], @position + 2)
+      end
+
+      def parameter_value(parameter_mode, position)
+        value = @data[position]
+        case parameter_mode
+        when nil, 0 then @data[value]
+        when 1 then value
+        end
+      end
+
+      def finished?
+        @finished
+      end
     end
   end
 end
--- 2019-ruby/spec/aoc/day7_spec.rb
+++ 2019-ruby/spec/aoc/day7_spec.rb
@@ -17,4 +17,173 @@ RSpec.describe AOC::Day7 do
       it { should eq 65210 }
     end
   end
+
+  describe 'part2' do
+    subject(:part2) { described_class.part2(StringIO.new(input)) }
+
+    context 'given "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"' do
+      let(:input) { "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5\n" }
+      it { should eq 139629729 }
+    end
+
+    context 'given "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"' do
+      let(:input) { "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10\n" }
+      it { should eq 18216 }
+    end
+  end
+
+  describe 'Program' do
+    subject(:program) { AOC::Day7::Program.new(state, input, output) }
+    let(:input) { [] }
+    let(:output) { [] }
+    before { program.process }
+
+    describe 'add' do
+      context 'given [1,0,0,0,99]' do
+        let(:state) { [1,0,0,0,99] }
+        its(:data) { should eq [2,0,0,0,99] }
+      end
+    end
+
+    describe 'multiply' do
+      context 'given [2,3,0,3,99]'do
+        let(:state) { [2,3,0,3,99] }
+        its(:data) { should eq [2,3,0,6,99] }
+      end
+
+      context 'given [2,4,4,5,99,0]' do
+        let(:state) { [2,4,4,5,99,0] }
+        its(:data) { should eq [2,4,4,5,99,9801] }
+      end
+    end
+
+    describe 'combinations' do
+      context 'given [1,1,1,4,99,5,6,0,99]' do
+        let(:state) { [1,1,1,4,99,5,6,0,99] }
+        its(:data) { should eq [30,1,1,4,2,5,6,0,99] }
+      end
+
+      context 'given [1,9,10,3,2,3,11,0,99,30,40,50]' do
+        let(:state) { [1,9,10,3,2,3,11,0,99,30,40,50] }
+        its(:data) { should eq [3500,9,10,70,2,3,11,0,99,30,40,50] }
+      end
+    end
+
+    describe 'parameter modes' do
+      context 'given [1002,4,3,4,33,99]' do
+        let(:state) { [1002, 4, 3, 4, 33, 99] }
+        its(:data) { should eq [1002, 4, 3, 4, 99, 99] }
+      end
+    end
+
+    describe 'output' do
+      context 'given [4,3,99,100]' do
+        let(:state) { [4, 3, 99, 100] }
+        its(:data) { should eq [4, 3, 99, 100] }
+        its(:output) { should eq [100] }
+      end
+    end
+
+    describe 'equals' do
+      context 'given [3,9,8,9,10,9,4,9,99,-1,8]' do
+        let(:state) { [3,9,8,9,10,9,4,9,99,-1,8] }
+
+        context 'input [8]' do
+          let(:input) { [8] }
+          its(:data) { should eq [3,9,8,9,10,9,4,9,99,1,8] }
+          its(:output) { should eq [1] }
+        end
+
+        context 'input [9]' do
+          let(:input) { [9] }
+          its(:data) { should eq [3,9,8,9,10,9,4,9,99,0,8] }
+          its(:output) { should eq [0] }
+        end
+      end
+
+      context 'given [3,3,1108,-1,8,3,4,3,99]' do
+        let(:state) { [3,3,1108,-1,8,3,4,3,99] }
+
+        context 'input [8]' do
+          let(:input) { [8] }
+          its(:data) { should eq [3,3,1108,1,8,3,4,3,99] }
+          its(:output) { should eq [1] }
+        end
+
+        context 'input [9]' do
+          let(:input) { [9] }
+          its(:data) { should eq [3,3,1108,0,8,3,4,3,99] }
+          its(:output) { should eq [0] }
+        end
+      end
+    end
+
+    describe 'less than' do
+      context 'given [3,9,7,9,10,9,4,9,99,-1,8]' do
+        let(:state) { [3,9,7,9,10,9,4,9,99,-1,8] }
+
+        context 'input [8]' do
+          let(:input) { [8] }
+          its(:data) { should eq [3,9,7,9,10,9,4,9,99,0,8] }
+          its(:output) { should eq [0] }
+        end
+
+        context 'input [7]' do
+          let(:input) { [7] }
+          its(:data) { should eq [3,9,7,9,10,9,4,9,99,1,8] }
+          its(:output) { should eq [1] }
+        end
+      end
+
+      context 'given [3,3,1107,-1,8,3,4,3,99]' do
+        let(:state) { [3,3,1107,-1,8,3,4,3,99] }
+
+        context 'input [8]' do
+          let(:input) { [8] }
+          its(:data) { should eq [3,3,1107,0,8,3,4,3,99] }
+          its(:output) { should eq [0] }
+        end
+
+        context 'input [7]' do
+          let(:input) { [7] }
+          its(:data) { should eq [3,3,1107,1,8,3,4,3,99] }
+          its(:output) { should eq [1] }
+        end
+      end
+    end
+
+    describe 'jumps' do
+      context 'given [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]' do
+        let(:state) { [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9] }
+
+        context 'input [0]' do
+          let(:input) { [0] }
+          its(:data) { should eq [3,12,6,12,15,1,13,14,13,4,13,99,0,0,1,9] }
+          its(:output) { should eq [0] }
+        end
+
+        context 'input [1]' do
+          let(:input) { [1] }
+          its(:data) { should eq [3,12,6,12,15,1,13,14,13,4,13,99,1,1,1,9] }
+          its(:output) { should eq [1] }
+        end
+      end
+
+      context 'given [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]' do
+        let(:state) { [3,3,1105,-1,9,1101,0,0,12,4,12,99,1] }
+
+        context 'input [0]' do
+          let(:input) { [0] }
+          its(:data) { should eq [3,3,1105,0,9,1101,0,0,12,4,12,99,0] }
+          its(:output) { should eq [0] }
+        end
+
+        context 'input [1]' do
+          let(:input) { [1] }
+          its(:data) { should eq [3,3,1105,1,9,1101,0,0,12,4,12,99,1] }
+          its(:output) { should eq [1] }
+        end
+      end
+    end
+  end
 end
